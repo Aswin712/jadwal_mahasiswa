5171b4fbb1c8550a7702605a80fb6f91121a74ce
Aswin712 <aswinsulistyo36762@gmail.com>
2026-03-26 17:28:15 +0700

    perbaikan format id

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/date_symbol_data_local.dart'; // ✅ Tambah import ini
 import 'app/modules/providers/app_provider.dart';
 import 'app/modules/services/notification_service.dart';
 import 'app/modules/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null); // ✅ Tambah baris ini
   await NotificationService().init();
   runApp(const StudentManagerApp());
 }
